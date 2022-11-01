10b60fdb668bb124ac85fd18d633526d6d59b471
sambaker149 <91675987+sambaker149@users.noreply.github.com>
2022-11-01 18:28:02 +0000

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MvcMovies.Models;
+
+namespace MvcMovies.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<MvcMovies.Models.Movie> Movie { get; set; }
+    }
+}
